e65f04e8eae9357d9c7f9ea34567df19a679a9df
yano-kentaro <ynkn0829@gmail.com>
2021-11-23 16:07:09 +0900

    junit設定

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,18 @@ module.exports = {
     '<rootDir>/pages/**/*.vue'
   ],
   coverageDirectory: 'coverage',
+  reporters: [
+    'default',
+    [
+      'jest-junit', {
+        suiteName: 'jest tests',
+        outputDirectory: 'reports/jest',
+        outputName: 'js-test-results.xml',
+        classNameTemplate: '{classname}-{title}',
+        titleTemplate: '{classname}-{title}',
+        ancestorSeparator: ' › ',
+      },
+    ],
+  ],
   testEnvironment: 'jsdom'
 }
